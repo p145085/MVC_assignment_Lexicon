e236eb9d595839338264b86d9290bb92f73a9571
emil.a.johansson <emil.a.johansson@gmail.com>
2022-11-03 17:38:38 +0100

    Added guessing-game.

--- Controllers/GuessController.cs
+++ Controllers/GuessController.cs
@@ -7,9 +7,29 @@ namespace MVC_assignment_Lexicon.Controllers
         [HttpGet]
         public IActionResult GuessView()
         {
-            //Random rnd = new Random();
-            //int rndTarget = rnd.Next(0, 100);
-            //Console.WriteLine(rndTarget);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GuessView(int guess)
+        {
+            Random rnd = new Random();
+            int rndTarget = rnd.Next(0, 100);
+
+            if (HttpContext.Session.GetInt32("rndNumber") == null)
+            {
+                HttpContext.Session.SetInt32("rndNumber", rndTarget);
+            }
+
+            if (guess == HttpContext.Session.GetInt32("rndNumber"))
+            {
+                ViewBag.Msg = "Correct.";
+            } else if (guess < HttpContext.Session.GetInt32("rndNumber"))
+            {
+                ViewBag.Msg = "Your number was lesser than the target.";
+            } else if (guess > HttpContext.Session.GetInt32("rndNumber"))
+            {
+                ViewBag.Msg = "Your number was greater than the target.";
+            }
             return View();
         }
     }
